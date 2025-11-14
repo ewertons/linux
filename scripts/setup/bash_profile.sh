b7d2a15019c9c4da2ccb493c7793ebe460e429c3
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2025-11-14 10:33:51 -0800

    Add script to setup user in bash

---
+++ scripts/setup/bash_profile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+echo "alias ll='ls -l'" >> ~/.profile
+echo "alias lll='ls -la'" >> ~/.profile
+echo "alias ..='cd ..'" >> ~/.profile
+echo "alias cmakedbg='cmake -DCMAKE_BUILD_TYPE=Debug'" >> ~/.profile
+echo "alias vgrnd='valgrind --leak-check=full --track-origins=yes'" >> ~/.profile
+echo "alias m='make -j'" >> ~/.profile
+echo "alias grep='grep --color=always'" >> ~/.profile
+
+echo ':set expandtab' >> ~/.vimrc
+echo ':set tabstop=4' >> ~/.vimrc
+echo ':set shiftwidth=4' >> ~/.vimrc
+echo ':set number' >> ~/.vimrc
+echo ':set hlsearch' >> ~/.vimrc
+
+echo 'set print pretty' >> ~/.gdbinit
+echo 'set pagination off' >> ~/.gdbinit
+echo 'set print elements 0' >> ~/.gdbinit
+
+
